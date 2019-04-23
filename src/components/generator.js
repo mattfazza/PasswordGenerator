5762130a57172e2309c2fe8903949791862648bc
mattfazza <matthewfazza@yahoo.com>
2019-04-22 23:42:04 -0600

    finished the logic of the generator component using states

--- src/components/generator.js
+++ src/components/generator.js
@@ -4,20 +4,22 @@ import { Button } from 'reactstrap';
 class Generator extends React.Component {
 
 
+    // var upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    // var lower = "qwertyuiopasdfghjklzxcvbnm"
+    // var combined = ''
 
     constructor(props) {
 
-
-        var numbers = "0123456789"
-        var specials = "!@#$%^&*()_+=-{}|\][:?><,."
-        var upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
-        var lower = "qwertyuiopasdfghjklzxcvbnm"
-        var combined = ""
 
         super(props)
 
         this.handleCheckbox = this.handleCheckbox.bind(this)
         this.generate = this.generate.bind(this)
+        this.numbers = "0123456789"
+        this.specials = "!@#$%^&*()_+=-{}|\][:?><,."
+        this.upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
+        this.lower = "qwertyuiopasdfghjklzxcvbnm"
+
 
         this.state = {
             checkboxGroup: {
@@ -32,12 +34,9 @@ class Generator extends React.Component {
         let obj = Object.assign(this.state.checkboxGroup)
         obj[event.target.value] = event.target.checked
         this.setState({ checkboxGroup: obj })
-        console.log(this.state.checkboxGroup);
-        console.log(this.numbers);
     }
 
     //TODO handle input box
-    //TODO handle button 'generate'
 
     getLength() {
         if (this.state.checkboxGroup.option1) {
@@ -47,19 +46,23 @@ class Generator extends React.Component {
         }
     }
 
-    generate() {
+    generate(e) {
+        e.preventDefault()
+        var combined = ''
+
         if (!this.state.checkboxGroup["option2"] && !this.state.checkboxGroup["option3"]) {
-            this.combined = this.numbers + this.lower
+            combined = this.numbers + this.lower + this.specials
         } else if (!this.state.checkboxGroup["option2"] && this.state.checkboxGroup["option3"]) {
-            this.combined = this.numbers + this.lower + this.upper
+            combined = this.numbers + this.lower + this.upper + this.specials
         } else {
-            this.combined = this.numbers + this.lower + this.upper + this.specials
+            combined = this.numbers + this.lower + this.upper
         }
 
-        // let realLength = this.getLength()
-        // let shuffled = this.combined.split("")
-        // var password = shuffled.substr(0, realLength)
+        let realLength = this.getLength()
+        let shuffled = combined.split('').sort(function () { return 0.5 - Math.random() }).join('')
+        let password = shuffled.substr(0, realLength)
 
+        document.getElementById("theInput").value = password;
 
     }
 
@@ -69,8 +72,8 @@ class Generator extends React.Component {
             <div>
                 Please, select what your password should look like, and click generate
                 <form>
-                    <input type="text"></input>
-                    <Button color="fff" style={{ color: "black" }}>Generate</Button>
+                    <input type="text" id="theInput"></input>
+                    <Button color="fff" style={{ color: "black" }} onClick={this.generate}>Generate</Button>
                     <p />
                     <input type="checkbox" name="checkboxGroup" value="option1" checked={this.state.checkboxGroup['option1']} onChange={this.handleCheckbox}></input>
                     Passoword must be between 8 and 16 characters
